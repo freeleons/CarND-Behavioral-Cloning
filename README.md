86c1918a0ebbbd36fd38163538ea7da2a6970bd2
Jue Wang's Github <freeleons@gmail.com>
2017-02-16 03:54:44 +0800

    Added the modified files

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # CarND-Behavioral-Cloning Project: Use Deep Learning to Clone Driving Behavior
-###At first, I really do not have any clues about what's the deep neural network architecture look alike to handle such a problem. It is a regression problem and not a classification problem. So I asked my mentor about the architecture of the neural network and my mentor recommended me a paper called ["End to End Learning for Self-Driving Cars"](https://images.nvidia.com/content/tegra/automotive/images/2016/solutions/pdf/end-to-end-dl-using-px.pdf)
+At first, I really do not have any clues about what's the deep neural network architecture look alike to handle such a problem. It is a regression problem and not a classification problem. So I asked my mentor about the architecture of the neural network and my mentor recommended me a paper called ["End to End Learning for Self-Driving Cars"](https://images.nvidia.com/content/tegra/automotive/images/2016/solutions/pdf/end-to-end-dl-using-px.pdf) 
+
+So I used their architecture to build the network. Here is what is look alike:
+![alt text](Architecture.png "Architecture")
+
+|Layer (type) |                    Output Shape    |      Param# 	|   Connected to|
+|-------------|------------------|------------------------------|---------------|
+|cropping2d_1 (Cropping2D)       | (None, 80, 320, 3) |   0     |      cropping2d_input_1[0][0]
+|lambda_1 (Lambda)             |(None, 40, 160, 3)   | 0     |      cropping2d_1[0][0]
+|batchnormalization_1 (BatchNorma |(None, 40, 160, 3)  |  160    |     lambda_1[0][0]
+|convolution2d_1 (Convolution2D)  |(None, 40, 160, 24) |  1824   |     batchnormalization_1[0][0]
+|maxpooling2d_1 (MaxPooling2D)    |(None, 20, 80, 24)  |  0      |     convolution2d_1[0][0]
+|spatialdropout2d_1 (SpatialDropo |(None, 20, 80, 24)  |  0      |     maxpooling2d_1[0][0]
+|convolution2d_2 (Convolution2D)  |(None, 20, 80, 36)  |  21636  |     spatialdropout2d_1[0][0]
+|maxpooling2d_2 (MaxPooling2D)    |(None, 10, 40, 36)  |  0      |     convolution2d_2[0][0]
+|spatialdropout2d_2 (SpatialDropo |(None, 10, 40, 36)  |  0      |     maxpooling2d_2[0][0]
+|convolution2d_3 (Convolution2D)  |(None, 10, 40, 48)  |  43248  |     spatialdropout2d_2[0][0]
+|maxpooling2d_3 (MaxPooling2D)    |(None, 5, 20, 48)   |  0      |     convolution2d_3[0][0]
+|spatialdropout2d_3 (SpatialDropo |(None, 5, 20, 48)   |  0      |     maxpooling2d_3[0][0]
+|convolution2d_4 (Convolution2D)  |(None, 5, 20, 64)   |  27712  |     spatialdropout2d_3[0][0]
+|maxpooling2d_4 (MaxPooling2D)    |(None, 3, 10, 64)   |  0      |     convolution2d_4[0][0]
+|spatialdropout2d_4 (SpatialDropo |(None, 3, 10, 64)   |  0      |     maxpooling2d_4[0][0]
+|convolution2d_5 (Convolution2D)  |(None, 3, 10, 64)   |  36928  |     spatialdropout2d_4[0][0]
+|maxpooling2d_5 (MaxPooling2D)    |(None, 2, 5, 64)    |  0      |     convolution2d_5[0][0]
+|spatialdropout2d_5 (SpatialDropo |(None, 2, 5, 64)    |  0      |     maxpooling2d_5[0][0]
+|flatten_1 (Flatten)              |(None, 640)         |  0      |     spatialdropout2d_5[0][0]
+|dense_1 (Dense)                  |(None, 100)         |  64100  |     flatten_1[0][0]
+|dense_2 (Dense)                  |(None, 50)          |  5050   |     dense_1[0][0]
+|dense_3 (Dense)                  |(None, 10)          |  510    |     dense_2[0][0]
+|dense_4 (Dense)                  |(None, 1)           |  11     |     dense_3[0][0]
+Total params: 201,179
+Trainable params: 201,099
+Non-trainable params: 80
+____________________________________________________________________________________________________
+
+However, when I tried to run the model on the simulator, it cannot last for a lap. I will run out of the road. I have tried 
+
+
+
+
+
+
+
--- model.py
+++ model.py
@@ -1,6 +1,8 @@
-import numpy as np
-import pickle
 import json
+import pandas as pd
+import numpy as np
+from scipy.misc import imread
+from sklearn.cross_validation import train_test_split
 from keras.models import Sequential, model_from_json
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Convolution2D, MaxPooling2D
@@ -11,131 +13,120 @@ from keras.layers.normalization import BatchNormalization
 from keras.layers.convolutional import Convolution2D, Cropping2D
 
 
-training_file = './data.pickle'
 
-with open(training_file, mode='rb') as f:
-    train = pickle.load(f)
 
-X_train, y_train = train['X_train'], train['y_train']
-X_test, y_test = train['X_test'], train['y_test']
+driving_log = pd.read_csv('driving_log.csv', index_col=False)
+driving_log.columns = ['Center', 'Left', 'Right', 'Steering', 'Throttle', 'Break', 'Speed']
 
-# def train_data_generator(X_train_arg, y_train_arg):
-#     for i in range(len(X_train_arg)):
-#         # yield ({'input': X_train_arg[i]}, {'output': y_train_arg[i]})
-#         yield (np.array([X_train_arg[i]]), np.array([y_train_arg[i]]))
+# 2. Prepare Data for Generator
+# X_train = np.array([]).reshape([-1,160,320,3])
+# X_train = np.array([])
+# y_train = np.array([])
 
+X_train = []
+y_train = []
+for index, content in driving_log.iterrows():
+
+    print(index)
+    print(content['Center'])
+    print(content['Steering'])
+
+    angle = content['Steering']
+    center = angle
+
+    X_train.append(content['Center'].strip())
+    y_train.append(center)
+
+    # angle = content['Steering']
+    # left = angle + 0.1
+    # right = angle - 0.1
+    #
+    # X_train.append(content['Left'].strip())
+    # y_train.append(left)
+    #
+    # X_train.append(content['Right'].strip())
+    # y_train.append(right)
+
+X_train, X_test_split, y_train, y_test = train_test_split(
+    X_train,
+    y_train,
+    test_size=0.001,
+    random_state=3)
+
+X_test = []
+for image in X_test_split:
+    print('Test:',image)
+    X_test.append(imread(image))
+
+
+#Write a generator to feed the data to the model to save memory
 def train_data_generator(X_train_arg, y_train_arg):
     while True:
         for i in range(len(X_train_arg)):
-            yield (np.array([X_train_arg[i]]).reshape([-1,160,320,3]), np.array([y_train_arg[i]]))
+            yield (np.array(imread(X_train_arg[i])).reshape([-1,160,320,3]), np.array([y_train_arg[i]]))
 
 
 num_of_train = len(X_train)
 num_of_angle = len(y_train)
 num_of_test = len(X_test)
 num_of_test_angle = len(y_test)
-image_shape = X_train[0].shape
 
-print("Image Shape:", image_shape)
 print("Number of Training Examples:", num_of_train)
 print("Number of Training Angles:", num_of_angle)
 print("Number of Testing Examples:", num_of_test)
 print("Number of Testing Angles:", num_of_test_angle)
 
-input_shape = X_train[0].shape
-print(input_shape, 'input shape')
 
-batch_size = 64 # The lower the better
-nb_classes = 1 # The output is a single digit: a steering angle
-nb_epoch = 5 # The higher the better
+nb_epoch = 1
 
 def resize(X):
-    # import tensorflow here so module is available when recreating pipeline from saved json.
     import tensorflow
     return tensorflow.image.resize_images(X, (40, 160))
 
-# import model and wieghts if exists
-try:
-	with open('model.json', 'r') as jfile:
-	    model = model_from_json(json.load(jfile))
 
-	# Use adam and mean squared error for training
-	model.compile("adam", "mse")
 
-	# import weights
-	model.load_weights('model.h5')
+model = Sequential()
+# Crop the horizon and car hood to remove the useless information
+model.add(Cropping2D(cropping=((60, 20), (0, 0)), input_shape=(160, 320, 3)))
+# Resize the images to 40X160 to improve performance
+model.add(BatchNormalization(axis=1))
+# Convolutional 5x5
+model.add(Convolution2D(24, 5, 5, border_mode='same', activation='relu'))
+model.add(MaxPooling2D(border_mode='same'))
+model.add(SpatialDropout2D(0.2))
+# Convolutional 5x5
+model.add(Convolution2D(36, 5, 5, border_mode='same', activation='relu'))
+model.add(MaxPooling2D(border_mode='same'))
+model.add(SpatialDropout2D(0.2))    # Conv 5x5
+model.add(Convolution2D(48, 5, 5, border_mode='same', activation='relu'))
+model.add(MaxPooling2D(border_mode='same'))
+model.add(SpatialDropout2D(0.2))
+# Convolutional 3x3
+model.add(Convolution2D(64, 3, 3, border_mode='same', activation='relu'))
+model.add(MaxPooling2D(border_mode='same'))
+model.add(SpatialDropout2D(0.2))
+# Convolutional 3x3
+model.add(Convolution2D(64, 3, 3, border_mode='same', activation='relu'))
+model.add(MaxPooling2D(border_mode='same'))
+model.add(SpatialDropout2D(0.2))
 
-	print("Imported model and weights")
+model.add(Flatten())
+# Fully Connected Layer
+model.add(Dense(100, activation='relu'))
+model.add(Dense(50, activation='relu'))
+model.add(Dense(10, activation='relu'))
+model.add(Dense(1))
 
-# If the model and weights do not exist, create a new model
-except:
-	# If model and weights do not exist in the local folder,
-	# initiate a model
 
-	# number of convolutional filters to use
-	nb_filters1 = 16
-	nb_filters2 = 8
-	nb_filters3 = 4
-	nb_filters4 = 2
-
-	# size of pooling area for max pooling
-	pool_size = (2, 2)
-
-	# convolution kernel size
-	kernel_size = (3, 3)
-
-	# Initiating the model
-	model = Sequential([
-    # Preprocess
-    # Crop above horizon and car hood to remove uneeded information
-    # Resize images to improve performance
-    # Normalize to keep weight values small with zero mean, improving numerical stability.
-    Cropping2D(cropping=((60, 20), (0, 0)), input_shape=(160, 320, 3)),
-    Lambda(resize),
-    BatchNormalization(axis=1),
-    # Conv 5x5
-    Convolution2D(24, 5, 5, border_mode='same', activation='elu'),
-    MaxPooling2D(border_mode='same'),
-    SpatialDropout2D(0.2),
-    # Conv 5x5
-    Convolution2D(36, 5, 5, border_mode='same', activation='elu'),
-    MaxPooling2D(border_mode='same'),
-    SpatialDropout2D(0.2),
-    # Conv 5x5
-    Convolution2D(48, 5, 5, border_mode='same', activation='elu'),
-    MaxPooling2D(border_mode='same'),
-    SpatialDropout2D(0.2),
-    # Conv 3x3
-    Convolution2D(64, 3, 3, border_mode='same', activation='elu'),
-    MaxPooling2D(border_mode='same'),
-    SpatialDropout2D(0.2),
-    # Conv 3x3
-    Convolution2D(64, 3, 3, border_mode='same', activation='elu'),
-    MaxPooling2D(border_mode='same'),
-    SpatialDropout2D(0.2),
-    # Flatten
-    Flatten(),
-    # Fully Connected
-    Dense(100, activation='elu', W_regularizer=l2(1e-6)),
-    Dense(50, activation='elu', W_regularizer=l2(1e-6)),
-    Dense(10, activation='elu', W_regularizer=l2(1e-6)),
-    Dense(1)
-    ])
-
-# Print out summary of the model
+# Printout the summary of the model
 model.summary()
 
-# Compile model using Adam optimizer
-# and loss computed by mean squared error
-model.compile(loss='mean_squared_error',
-              optimizer=Adam(),
+# Compile model using Adam optimizer and loss computed by mean squared error
+model.compile(loss='mse',
+              optimizer=Adam(lr=0.0003, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0),
               metrics=['accuracy'])
 
-### Model training
-# history = model.fit(X_train, y_train,
-                    # batch_size=batch_size, nb_epoch=nb_epoch,
-                    # verbose=1, validation_split = 0.1)
-
+# Training the Model
 history = model.fit_generator(train_data_generator(X_train, y_train), samples_per_epoch = len(X_train), nb_epoch=nb_epoch, verbose=1)
 score = model.evaluate(np.array([X_test]).reshape([-1,160,320,3]), np.array([y_test]).reshape(-1, 1), verbose=0)
 print('Test score:', score[0])
@@ -145,33 +136,10 @@ import json
 import os
 import h5py
 
-# Save the model.
-# If the model.json file already exists in the local file,
-# warn the user to make sure if user wants to overwrite the model.
-if 'model.json' in os.listdir():
-	print("The file already exists")
-	print("Want to overwite? y or n")
-	user_input = input()
-
-	if user_input == "y":
-		# Save model as json file
-		json_string = model.to_json()
-
-		with open('model.json', 'w') as outfile:
-			json.dump(json_string, outfile)
-
-			# save weights
-			model.save_weights('./model.h5')
-			print("Overwrite Successful")
-	else:
-		print("the model is not saved")
-else:
-	# Save model as json file
-	json_string = model.to_json()
-
-	with open('model.json', 'w') as outfile:
-		json.dump(json_string, outfile)
-
-		# save weights
-		model.save_weights('./model.h5')
-		print("Saved")
+# Save the model. Save the model as json file
+json_string = model.to_json()
+with open('model.json', 'w') as outfile:
+	json.dump(json_string, outfile)
+	# save weights
+	model.save_weights('./model.h5')
+	print("Saved")
